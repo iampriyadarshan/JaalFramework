3c5a8d8944c3a31317ca17804fa2be89d9ad4343
Priyadarshan <priyadarshan0216@gmail.com>
2024-03-18 18:41:06 +0530

    JaalFramework

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Jaal",
-            targets: ["Jaal"]),
+            targets: ["Framework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Jaal"),
+      .binaryTarget(name: "Framework", path: "Sources/Framework/Jaal.xcframework"),
         .testTarget(
             name: "JaalTests",
-            dependencies: ["Jaal"]),
+            dependencies: ["Framework"]),
     ]
 )
