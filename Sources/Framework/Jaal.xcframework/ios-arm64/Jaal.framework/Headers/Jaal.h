a6084fc4c2c228388ecaeb39b0df580835b13000
Priyadarshan <priyadarshan0216@gmail.com>
2024-03-18 18:40:18 +0530

    JaalFramework

---
+++ Sources/Framework/Jaal.xcframework/ios-arm64/Jaal.framework/Headers/Jaal.h
@@ -0,0 +1,18 @@
+//
+//  Jaal.h
+//  Jaal
+//
+//  Created by Priyadarshan Meshram on 18/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jaal.
+FOUNDATION_EXPORT double JaalVersionNumber;
+
+//! Project version string for Jaal.
+FOUNDATION_EXPORT const unsigned char JaalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jaal/PublicHeader.h>
+
+
